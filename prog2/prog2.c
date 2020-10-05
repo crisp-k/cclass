bde7fc38f91247967d1b594b3ca2a7a54c9c27c1
crisp-k <53191886+crisp-k@users.noreply.github.com>
2020-10-04 22:58:14 -0400

    Documentation, output file correct

--- prog2/prog2.c
+++ prog2/prog2.c
@@ -1,7 +1,11 @@
+/* COP 3502C Assignment 2
+This program is written by: Kyle Crisp */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include "leak_detector_c.h"
 
 #define MAXCHAR 51
 #define MAXGROUNDS 10
@@ -35,7 +39,7 @@ typedef struct soldierNode
 int readNumGrounds(FILE* input)
 {
     int numGrounds;
-            
+    
     fscanf(input, "%d", &numGrounds);
 
     return numGrounds;
@@ -81,7 +85,9 @@ struct killQueue* initQueue(void)
 {
     struct killQueue *initialQueue = (struct killQueue*) malloc(sizeof(struct killQueue));
 
+    // numElements is used as a catch to break in phaseTwo
     initialQueue->numElements = 0;
+
     initialQueue->head = NULL;
     initialQueue->tail = NULL;
 
@@ -97,28 +103,34 @@ struct killQueue enQueue(killQueue queue, int soldierNumber)
     soldier->last = NULL;
     soldier->next = NULL;
 
-
-    if(queue.head == NULL)
+    if(queue.head == NULL)  // Block for queues with no elements
     {
         queue.head = soldier;
         queue.tail = soldier;
     }
-    else
+    else    // Otherwise insert element at end of list
     {
         traverseNode = queue.head;
+
         while(traverseNode != queue.tail)
             {
-                traverseNode = traverseNode->next;
+                traverseNode = traverseNode->next;      // Begins traversing to the queue tail
             }
-
+        
         traverseNode->next = soldier;
-        queue.tail = soldier;
+
+        // Links new element to tail
         soldier->next = queue.head;
         soldier->last = traverseNode;
-        queue.head->last = soldier;
-        
+
+        // Makes new element the tail, and connects the circular list together
+        queue.tail = soldier;
+        queue.head->last = queue.tail;
     }
-    
+
+    soldier = NULL;
+    free(soldier);
+
     return queue;
 }
 
@@ -126,35 +138,41 @@ struct killQueue* fillQueue(int numSoldiers)
 {
     struct killQueue* queueToFill = (struct killQueue*) malloc(numSoldiers * (sizeof(struct killQueue)));
 
+    // Using double loop so the queue is filled with descending ints from numSoldiers to 0
     for(int i = 0; i < numSoldiers; i++)
     {
-        for(int j = numSoldiers; j > 0; j--)
+        for(int soldierNum = numSoldiers; soldierNum > 0; soldierNum--)
         {
-            queueToFill[i] = enQueue(queueToFill[i], j);
+            queueToFill[i] = enQueue(queueToFill[i], soldierNum);
         }
     }
 
     return queueToFill;
 }
 
-void display(killQueue queue)
+void display(killQueue queue, FILE *output)
 {
     soldierNode *traverseNode;
 
     traverseNode = queue.head;
 
-    while(traverseNode != queue.tail)
+    while(traverseNode != queue.tail)   // Travels the queue and prints each value
     {
         printf(" %d", traverseNode->soldierNum);
+        fprintf(output, " %d", traverseNode->soldierNum);
         traverseNode = traverseNode->next;
     }
 
+    // Because the loop stops just before the tail,
+    // a final printf is used to print the tails' data
     printf(" %d", queue.tail->soldierNum);
+    fprintf(output, " %d", queue.tail->soldierNum);
+    
 }
 
 bool isEmpty(killQueue *queue)
 {
-    if(queue->numElements == 0)
+    if(queue->numElements == 0) 
     {
         return true;
     }
@@ -164,19 +182,9 @@ bool isEmpty(killQueue *queue)
     }
 }
 
-void formattedDisplay(killQueue **groundQueueList)
-{
-    for(int i = 0; i < MAXGROUNDS; i++)
-    {
-        if(!isEmpty(groundQueueList[i]))
-        {
-            printf("\n%i %s", groundQueueList[i]->queueInformation->groundNumber,
-                              groundQueueList[i]->queueInformation->groundName);
-            display(*groundQueueList[i]);
-        }
-    }
-}
-
+// Function copies information into the killQueue queues
+// This is done because the killQueue array is in the desired order in relation to ground numbers,
+// while the original groundInfo array is not in order in relation to ground numbers
 struct groundInfo* groundInformationTransfer(struct groundInfo initialInfo)
 {
     groundInfo* queueInformation = (struct groundInfo*) malloc(sizeof(struct groundInfo));
@@ -199,46 +207,57 @@ void reverseQueueOrder(killQueue *queue)
     leadingNode = queue->head->next;
     trailingNode = queue->head;
 
-    while(leadingNode != queue->tail)
+    while(leadingNode != queue->tail) // Traverses through queue and swaps the node pointers to reverse the order
     {
         trailingNode->next = trailingNode->last;
         trailingNode->last = leadingNode;
         trailingNode = leadingNode;
         leadingNode = leadingNode->next;
     }
-    trailingNode->next = trailingNode->last;
-    trailingNode->last = leadingNode;
-
+    trailingNode->next = trailingNode->last; // Swaps node pointers for tail and node just before tail
+    trailingNode->last = leadingNode;        
+                                            
     leadingNode->next = trailingNode;
     leadingNode->last = queue->head;
 
-    queue->tail = queue->head;
+    queue->tail = queue->head;  // Swaps queue and tail designations
     queue->head = leadingNode;
 }
 
-void phaseOne(killQueue *queue)
+void phaseOne(killQueue *queue, FILE *outfile)
 {
     soldierNode *killNode;
-    soldierNode *deadSoldier = (soldierNode*) malloc(sizeof(soldierNode));
+    soldierNode *deadSoldier;
+
+    // Creates new variables to store information for readability purposes
     int soldiers = queue->queueInformation->numSoldiers;
     int keepAlive = queue->queueInformation->numKeepAlive;
     int killSkip = queue->queueInformation->numToSkip;
 
-    killNode = queue->head;
+    killNode = queue->head; // Points to head...
 
+    // and begins moving through queue based on the number
+    // of soldiers to skip in a given ground.
+    // This intial loop is for the first execution because
+    // killNode is pointing to the head to begin with, and that counts as a step
     for(int i = 1; i < (killSkip - 1); i++)
-    {
+    {                                          
         killNode = killNode->next;
     }
 
+    // Isolates deadSoldier node by moving node pointers
     deadSoldier = killNode->next;
     killNode->next = killNode->next->next;
     deadSoldier->next->last = killNode;
 
-    printf("\nSoldier# %d executed\n", deadSoldier->soldierNum);
-    free(deadSoldier);
 
-    queue->numElements -= 1;
+    printf("\nSoldier# %d executed\n", deadSoldier->soldierNum);
+    fprintf(outfile, "\nSoldier# %d executed\n", deadSoldier->soldierNum);
+
+    free(deadSoldier);
+    queue->numElements -= 1; // Counter to keep track of how many elements are in the queue currently
+
+
 
     // For loop conditional check is -1 due to the initial execution
     // before this kill block begins
@@ -249,26 +268,27 @@ void phaseOne(killQueue *queue)
             killNode = killNode->next;
         }
         
+        // Isolates deadSoldier node by moving node pointers
         deadSoldier = killNode->next;
         killNode->next = deadSoldier->next;
         deadSoldier->next->last = killNode;
-        
+
+
         if(deadSoldier == queue->head)
         {
-            queue->head = deadSoldier->next; 
+            queue->head = deadSoldier->next;    // Move head designation to one node infront of deadSoldier
         }
         else if(deadSoldier == queue->tail)
         {
-            queue->tail = killNode;
+            queue->tail = killNode; // Move tail designation to one node behind deadSoldier
         }
 
         printf("Soldier# %d executed\n", deadSoldier->soldierNum);
+        fprintf(outfile, "Soldier# %d executed\n", deadSoldier->soldierNum);
 
         free(deadSoldier);
         queue->numElements -= 1;
     }
-
-    printf("Number of elements in queue: %i\n", queue->numElements);
 }
 
 int peek(killQueue *queue)
@@ -289,25 +309,26 @@ int findGroundIndexOfMax(killQueue **groundQueueList)
     bool findLowestGroundNumber;
     max = -1;
 
+
     for(int i = 0; i < MAXGROUNDS; i++)
     {
-        if(!isEmpty(groundQueueList[i]))
+        if(!isEmpty(groundQueueList[i]))    // If queue has members
         {
-            temp = peek(groundQueueList[i]);
-            printf("peak: %i from line %i\n", temp, i);
-            if(max < temp)
+            temp = peek(groundQueueList[i]);  
+
+            if(max < temp) // Find max value
             {
                 max = temp;
                 groundIndex = i;
             }
             else if(max == temp)
             {
-                findLowestGroundNumber = true;
+                findLowestGroundNumber = true;  // Conditional to account for when the queue array needs to be looped again
             }
         }
     }
 
-    if(findLowestGroundNumber)
+    if(findLowestGroundNumber)  // Finds first ground with desired max value
     {
         for(int i = 0; i < MAXGROUNDS; i++)
         {
@@ -321,19 +342,18 @@ int findGroundIndexOfMax(killQueue **groundQueueList)
         }  
     } 
 
-    printf("Line to dequeue from: %i\n", groundIndex);
-
     return groundIndex;
 }
 
-void deQueue(killQueue *queue)
+// deQueue used in phaseTwo
+void deQueue(killQueue *queue, FILE *output)
 {
     soldierNode *deadSoldier;
 
     deadSoldier = queue->head;
 
     queue->head = queue->head->next;
-    if(queue->head != queue->tail)
+    if(queue->head != queue->tail)  // Conditonal to manage deletion of nodes that are neither the head or tail
     {
         queue->head->last = deadSoldier->last;
         deadSoldier->last->next = queue->head;
@@ -343,11 +363,13 @@ void deQueue(killQueue *queue)
     queue->numElements -= 1;
 
     printf("Executed Soldier %i", deadSoldier->soldierNum);
+    fprintf(output, "Executed Soldier %i", deadSoldier->soldierNum);
 }
 
 int countQueuesWithMembers(killQueue **groundQueueList)
 { 
     int count = 0;
+
     for(int i = 0; i < MAXGROUNDS; i++)
     {
         if(!isEmpty(groundQueueList[i]))
@@ -357,34 +379,55 @@ int countQueuesWithMembers(killQueue **groundQueueList)
     return count;
 }
 
-void phaseTwo(killQueue **groundQueueList)
+void phaseTwo(killQueue **groundQueueList, FILE *output)
 {
     bool lastSoldier = false;
     int groundIndex = 0;
 
-    while(countQueuesWithMembers(groundQueueList) >= 1)
+    while(!lastSoldier)
     {
-        
-        groundIndex = findGroundIndexOfMax(groundQueueList);
+        groundIndex = findGroundIndexOfMax(groundQueueList); // Finds index of array with max soldier nubmer
 
-        deQueue(groundQueueList[groundIndex]);
+        deQueue(groundQueueList[groundIndex], output);  // Executes that soldier
         printf(" from line %i\n", groundIndex);
+        fprintf(output, " from line %i\n", groundIndex);
 
-        printf("%i\n", groundQueueList[groundIndex]->numElements);
-
+        // Checks if we have found the survivor or not
         if(countQueuesWithMembers(groundQueueList) == 1 && groundQueueList[groundIndex]->numElements == 1)
-            break;
+        {
+            lastSoldier = true;
+        }   
     }
-        printf("Soldier %i from line %i will survive\n", groundQueueList[groundIndex]->head->soldierNum,
-                                                         groundIndex);
+
+        printf("\nSoldier %i from line %i will survive\n", groundQueueList[groundIndex]->head->soldierNum,
+                                                           groundIndex);
+        fprintf(output, "\nSoldier %i from line %i will survive\n", groundQueueList[groundIndex]->head->soldierNum,
+                                                                    groundIndex);
+
+        free(groundQueueList[groundIndex]->head);
 }
 
+
+void formattedOutput(killQueue **groundQueueList, FILE *output)
+{
+    for(int i = 0; i < MAXGROUNDS; i++)
+    {
+        if(!isEmpty(groundQueueList[i]))
+        {
+            printf("\n%i %s", groundQueueList[i]->queueInformation->groundNumber,
+                              groundQueueList[i]->queueInformation->groundName);
+            fprintf(output, "\n%i %s", groundQueueList[i]->queueInformation->groundNumber,
+                                       groundQueueList[i]->queueInformation->groundName);                  
+            display(*groundQueueList[i], output);
+        }
+    }
+}
 int main(void)
 {
+    atexit(report_mem_leak);
+
     FILE *input, *output;
-    char groundName[MAXCHAR];
-    int numGrounds, groundNumber;
-    int numToSkip, numKeepAlive;
+    int numGrounds;
 
     groundInfo *groundInformation;
     killQueue **groundQueueList;
@@ -414,6 +457,7 @@ int main(void)
 
 
         // Initalizes each queue with appropiate data read from input file
+        // Places queue in index specified by ground number
         for(int i = 0; i < MAXGROUNDS; i++)
         {
             groundQueueList[i] = initQueue();
@@ -428,34 +472,35 @@ int main(void)
                 }                
             }
         }   
-        
-        printf("\nInitial nonempty lists status");
-        for(int i = 0; i < MAXGROUNDS; i++)
-        {
-            if(!isEmpty(groundQueueList[i]))
-            {
-                printf("\n%i %s", groundQueueList[i]->queueInformation->groundNumber,
-                                  groundQueueList[i]->queueInformation->groundName);
-                display(*groundQueueList[i]);
 
+        printf("Initial nonempty lists status");
+        fprintf(output, "\nInitial nonempty lists status");
+        formattedOutput(groundQueueList, output);
+
+        for(int i = 0; i < MAXGROUNDS; i++)
+            if(!isEmpty(groundQueueList[i]))
                 reverseQueueOrder(groundQueueList[i]);
-            }
-            
-        }
         
         printf("\n\nAfter ordering nonempty lists status");
+        fprintf(output, "\n\nAfter ordering nonempty lists status");
+        formattedOutput(groundQueueList, output);
+
+        printf("\nPhase1 execution\n");
+        fprintf(output, "\n\nPhase1 execution\n");
+        // Begin PhaseOne
         for(int i = 0; i < MAXGROUNDS; i++)
         {
             if(!isEmpty(groundQueueList[i]))
             {
-                printf("\n%i %s", groundQueueList[i]->queueInformation->groundNumber,
-                                  groundQueueList[i]->queueInformation->groundName);
-                display(*groundQueueList[i]);
-                
-                phaseOne(groundQueueList[i]);
+                printf("Line# %i %s\n", i, groundQueueList[i]->queueInformation->groundName);
+                fprintf(output, "\nLine# %i %s", i, groundQueueList[i]->queueInformation->groundName);
+                phaseOne(groundQueueList[i], output); 
             }
         }
-        phaseTwo(groundQueueList);   
+
+        printf("\nPhase2 execution\n");
+        fprintf(output, "\nPhase2 execution\n");
+        phaseTwo(groundQueueList, output);   
 
     }
     return 0;
